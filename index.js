73d1a6ccd8a5b28c05e25f1fcb06d6b08d955c35
Jonathan St-Onge <jonathanstonge7@gmail.com>
2024-11-26 10:49:33 -0500

    more cleaning

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
-import { descending } from "d3-array";
-
 import combElems from './src/combine_distributions.js'
 import rank_turbulence_divergence from './src/rank_turbulence_divergence.js'
 import diamond_count from './src/diamond_count.js'
 
-import { getUnions, setdiff, rin, matlab_sort, tiedrank, which, rank_maxlog10, zeros } from './src/utils_helpers.js'
-
-
-function balanceDat(elem1, elem2) {
-  const types_1 = elem1.map(d => d.types)
-  const types_2 = elem2.map(d => d.types)
-  
-  const union_types = getUnions(types_1, types_2)
-  const tot_types = types_1.length+types_2.length
-  
-  return [ 
-    { y_coord: "total count",     frequency: +(types_2.length / tot_types).toFixed(3) },
-    { y_coord: "total count",     frequency: -(types_1.length / tot_types).toFixed(3) },
-    { y_coord: "all names",       frequency: +(types_2.length / union_types.size).toFixed(3) },
-    { y_coord: "all names",       frequency: -(types_1.length / union_types.size).toFixed(3) },
-    { y_coord: "exclusive names", frequency: +(setdiff(types_2, types_1).size / types_2.length).toFixed(3) },
-    { y_coord: "exclusive names", frequency: -(setdiff(types_1, types_2).size / types_1.length).toFixed(3) } 
-  ]
-}
-   
-function wordShift_dat(me, dat) { 
-  const out = []
-  for (let i=0; i < me[0]['types'].length; i++) {
-    const rank_diff = me[0]['ranks'][i]-me[1]['ranks'][i]
-    out.push({
-      'type': `${me[0]['types'][i]} (${me[0]['ranks'][i]} ⇋ ${me[1]['ranks'][i]})` ,
-      'rank_diff': rank_diff,
-      'metric': rank_diff < 0 ? -dat.deltas[i] : dat.deltas[i], 
-    })
-  }
-  
-  return out.slice().sort((a, b) => descending(Math.abs(a.metric), Math.abs(b.metric)))
-}
-
+import { rin, matlab_sort, tiedrank, which, rank_maxlog10, zeros, balanceDat, wordShift_dat } from './src/utils_helpers.js'
 
 export{
   rank_maxlog10,
--- src/utils_helpers.js
+++ src/utils_helpers.js
@@ -1,20 +1,22 @@
-export { matlab_sort, rin, rank_maxlog10, tiedrank, which, zeros, getUnions, setdiff } ;
+export { matlab_sort, rin, rank_maxlog10, tiedrank, which, zeros, getUnions, setdiff, balanceDat, wordShift_dat } ;
 
-  // Takes arrays, returns a Set object containing the union of both arrays
-   function getUnions(x,y) {
-    let a = new Set(x); // convert array x to a Set object
-    let b = new Set(y); // convert array y to a Set object
-    return new Set([...a, ...b]); // return a new Set object containing the union of a and b
-  }
-  
-  // Takes arrays, returns a Set object
-  function setdiff(x,y) {
-    let a = new Set(x); // convert array x to a Set object
-    let b = new Set(y); // convert array y to a Set object
-    // return a new Set object containing elements in a that are not present in b
-    return new Set(       
-      [...a].filter(x => !b.has(x)));
-  } 
+import { descending } from "d3-array";
+
+// Takes arrays, returns a Set object containing the union of both arrays
+ function getUnions(x,y) {
+  let a = new Set(x); // convert array x to a Set object
+  let b = new Set(y); // convert array y to a Set object
+  return new Set([...a, ...b]); // return a new Set object containing the union of a and b
+}
+
+// Takes arrays, returns a Set object
+function setdiff(x,y) {
+  let a = new Set(x); // convert array x to a Set object
+  let b = new Set(y); // convert array y to a Set object
+  // return a new Set object containing elements in a that are not present in b
+  return new Set(       
+    [...a].filter(x => !b.has(x)));
+} 
 
 function which(x) {
   // Which indices are TRUE?
@@ -98,4 +100,38 @@ function zeros(length){
   let empty_mat = createArray(length,length)
   return Array.from(empty_mat, arr => arr.fill(0))
 }
+
+// helpers to wrangle data for the balance plot
+function balanceDat(elem1, elem2) {
+  const types_1 = elem1.map(d => d.types)
+  const types_2 = elem2.map(d => d.types)
+  
+  const union_types = getUnions(types_1, types_2)
+  const tot_types = types_1.length+types_2.length
+  
+  return [ 
+    { y_coord: "total count",     frequency: +(types_2.length / tot_types).toFixed(3) },
+    { y_coord: "total count",     frequency: -(types_1.length / tot_types).toFixed(3) },
+    { y_coord: "all names",       frequency: +(types_2.length / union_types.size).toFixed(3) },
+    { y_coord: "all names",       frequency: -(types_1.length / union_types.size).toFixed(3) },
+    { y_coord: "exclusive names", frequency: +(setdiff(types_2, types_1).size / types_2.length).toFixed(3) },
+    { y_coord: "exclusive names", frequency: -(setdiff(types_1, types_2).size / types_1.length).toFixed(3) } 
+  ]
+}
+   
+// helper to wrangle the data for the wordshift plot
+function wordShift_dat(me, dat) { 
+  const out = []
+  for (let i=0; i < me[0]['types'].length; i++) {
+    const rank_diff = me[0]['ranks'][i]-me[1]['ranks'][i]
+    out.push({
+      'type': `${me[0]['types'][i]} (${me[0]['ranks'][i]} ⇋ ${me[1]['ranks'][i]})` ,
+      'rank_diff': rank_diff,
+      'metric': rank_diff < 0 ? -dat.deltas[i] : dat.deltas[i], 
+    })
+  }
+  
+  return out.slice().sort((a, b) => descending(Math.abs(a.metric), Math.abs(b.metric)))
+}
+
 
